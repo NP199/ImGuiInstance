a0d344a840047d81831f0b4a501463b5a358094d
niklas <niklas.pierick@stud.thga.de>
2024-10-02 22:39:18 +0200

    Hinzzufügen eines neuen Konstruktors

--- src/ImGuiInstance/ImGuiInstance.hpp
+++ src/ImGuiInstance/ImGuiInstance.hpp
@@ -43,6 +43,7 @@ public:
             std::terminate();
         }
 
+
         glfwMakeContextCurrent(window);
         glfwSwapInterval(1);
 
@@ -61,6 +62,61 @@ public:
         ImPlot::CreateContext();
         [[maybe_unused]] ImGuiIO& io = ImGui::GetIO();
         ImGui::StyleColorsDark();
+
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init(glsl_version);
+    }
+
+    template<typename Lambda, typename Lambda2>
+    ImGuiInstance(std::size_t width, std::size_t height, std::string const& windowName, Lambda lambda, Lambda2 lambda2) {
+        glfwSetErrorCallback([](int error, char const* description) {
+            std::fprintf(stderr, "glfw error %d: %s\n", error, description);
+            std::terminate();
+        });
+
+        if(!glfwInit()) {
+            std::fprintf(stderr, "glfwInit failed\n");
+            std::terminate();
+        }
+
+        // GL 3.0 + GLSL 130
+        char const* const glsl_version = "#version 130";
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+        window = glfwCreateWindow(
+          static_cast<int>(width),
+          static_cast<int>(height),
+          windowName.c_str(),
+          nullptr,
+          nullptr);
+        if(window == nullptr) {
+            std::fprintf(stderr, "glfwCreateWindow failed\n");
+            std::terminate();
+        }
+
+        lambda(window);
+
+        glfwMakeContextCurrent(window);
+        glfwSwapInterval(1);
+
+        if(!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress))) {
+            std::fprintf(stderr, "gladLoadGLLoader failed\n");
+            std::terminate();
+        }
+
+        int screen_width;
+        int screen_height;
+        glfwGetFramebufferSize(window, &screen_width, &screen_height);
+        glViewport(0, 0, screen_width, screen_height);
+
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImPlot::CreateContext();
+        [[maybe_unused]] ImGuiIO& io = ImGui::GetIO();
+        ImGui::StyleColorsDark();
+
+        lambda2();
 
         ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init(glsl_version);
